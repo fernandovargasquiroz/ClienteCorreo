320b738e774c89060b6f2f877cf39336b91b0549
Favio <Favio@10.0.0.15>
2019-02-16 16:46:57 -0400

    cliente

--- Cliente/src/cliente/Cliente.java
+++ Cliente/src/cliente/Cliente.java
@@ -16,7 +16,7 @@ public class Cliente {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("");
+        System.out.println("Test email Cliente");
     }
     
 }
